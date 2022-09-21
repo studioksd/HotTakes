05a11c706f6e06358ae6b1a97511809d2da9c006
Kentin <kentin.sengsavanh@gmail.com>
2022-09-21 17:29:43 +0200

    Mise en place de l'authentification

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://ksd:ksdksd@cluster0.y8qvrim.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
